5ad801c0871711acb401d17348596a0785fb38de
Jon Seddon <jon.seddon@metoffice.gov.uk>
2022-06-13 11:13:32 +0100

    Update for Pylint 2.14 and 2.13

--- dmt_app/__init__.py
+++ dmt_app/__init__.py
@@ -3,3 +3,5 @@
 # This file is part of cube_helper and is released under the
 # BSD 3-Clause license.
 # See LICENSE in the root of the repository for full licensing details.
+
+# pylint: disable=django-not-configured
