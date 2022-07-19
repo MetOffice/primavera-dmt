b1d0242ea1de628947ee9a7d29493ba6adaf250a
Jon Seddon <jon.seddon@metoffice.gov.uk>
2022-07-19 14:37:17 +0100

    Correct project name

--- dmt_app/utils/__init__.py
+++ dmt_app/utils/__init__.py
@@ -1,5 +1,5 @@
 # (C) Crown Copyright, Met Office. All rights reserved.
 #
-# This file is part of cube_helper and is released under the
+# This file is part of primavera-dmt and is released under the
 # BSD 3-Clause license.
 # See LICENSE in the root of the repository for full licensing details.
